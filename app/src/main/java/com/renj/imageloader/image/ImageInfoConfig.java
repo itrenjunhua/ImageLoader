394c97ebcf26976c772257ae44f1aa852fef58b5
Renj <itrenjunhua@163.com>
2018-03-08 10:51:49 +0800

    定义图片加载框架二次封装基本类

---
+++ app/src/main/java/com/renj/imageloader/image/ImageInfoConfig.java
@@ -0,0 +1,28 @@
+package com.renj.imageloader.image;
+
+/**
+ * ======================================================================
+ * <p>
+ * 作者：Renj
+ * <p>
+ * 创建时间：2018-03-08   10:32
+ * <p>
+ * 描述：加载图片时的信息配置
+ * <p>
+ * 修订历史：
+ * <p>
+ * ======================================================================
+ */
+public class ImageInfoConfig {
+    private ImageInfoConfig() {
+    }
+
+    public static class Builder {
+        public Builder() {
+        }
+
+        public ImageInfoConfig build() {
+            return new ImageInfoConfig();
+        }
+    }
+}
