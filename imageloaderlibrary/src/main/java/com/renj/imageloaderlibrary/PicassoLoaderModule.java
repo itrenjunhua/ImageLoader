772ef1c7eeece7d96bb065cbb25066a4fc08850a
Renj <itrenjunhua@163.com>
2018-04-27 17:33:51 +0800

    导入Picasso库，增加Picasso实现类

---
+++ imageloaderlibrary/src/main/java/com/renj/imageloaderlibrary/PicassoLoaderModule.java
@@ -0,0 +1,41 @@
+package com.renj.imageloaderlibrary;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.ImageView;
+
+import com.renj.imageloaderlibrary.loader.IImageLoaderModule;
+import com.renj.imageloaderlibrary.loader.ImageInfoConfig;
+
+/**
+ * ======================================================================
+ * <p>
+ * 作者：Renj
+ * 邮箱：renjunhua@anlovek.com
+ * <p>
+ * 创建时间：2018-04-25   15:51
+ * <p>
+ * 描述：使用Picasso框架加载图片
+ * <p>
+ * 修订历史：
+ * <p>
+ * ======================================================================
+ */
+public class PicassoLoaderModule implements IImageLoaderModule {
+    private Application application;
+
+    @Override
+    public void init(Application application) {
+        this.application = application;
+    }
+
+    @Override
+    public void loadImage(@NonNull Context context, @NonNull String url, @NonNull ImageView imageView) {
+    }
+
+    @Override
+    public void loadImage(@NonNull ImageInfoConfig imageInfoConfig) {
+
+    }
+}
