9ee3e361e59f4f0d5aa394cb718d89520b9ba846
Renj <itrenjunhua@163.com>
2018-05-27 16:32:27 +0800

    PicassoLoaderModule最近基本形式的加载图片完成

--- imageloaderlibrary/src/main/java/com/renj/imageloaderlibrary/picasso/PicassoLoaderModule.java
+++ imageloaderlibrary/src/main/java/com/renj/imageloaderlibrary/picasso/PicassoLoaderModule.java
@@ -7,6 +7,8 @@ import android.widget.ImageView;
 
 import com.renj.imageloaderlibrary.loader.IImageLoaderModule;
 import com.renj.imageloaderlibrary.loader.ImageInfoConfig;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
 
 /**
  * ======================================================================
@@ -32,10 +34,69 @@ public class PicassoLoaderModule implements IImageLoaderModule {
 
     @Override
     public void loadImage(@NonNull Context context, @NonNull String url, @NonNull ImageView imageView) {
+        ImageInfoConfig imageInfoConfig = new ImageInfoConfig.Builder()
+                .context(context)
+                .url(url)
+                .target(imageView)
+                .build();
+        loadImage(imageInfoConfig);
     }
 
     @Override
     public void loadImage(@NonNull ImageInfoConfig imageInfoConfig) {
+        Picasso picasso = createPicasso(imageInfoConfig);
+        RequestCreator requestCreator = loadPath(picasso, imageInfoConfig);
+        if (imageInfoConfig.getTarget() instanceof ImageView) {
+            requestCreator.into((ImageView) imageInfoConfig.getTarget());
+        }
+    }
 
+    /**
+     * 确定图片加载路径
+     */
+    @NonNull
+    private RequestCreator loadPath(Picasso picasso, @NonNull ImageInfoConfig imageInfoConfig) {
+        if (imageInfoConfig.getFile() != null)
+            return picasso.load(imageInfoConfig.getFile());
+
+        if (imageInfoConfig.getDrawableId() > 0)
+            return picasso.load(imageInfoConfig.getDrawableId());
+
+        if (imageInfoConfig.getUri() != null)
+            return picasso.load(imageInfoConfig.getUri());
+
+        // imageInfoConfig.getUrl() 也可能为 null
+        return picasso.load(imageInfoConfig.getUrl());
+    }
+
+    /**
+     * 调用 {@link Picasso#with(Context)} 方法创建 {@link Picasso} 对象
+     *
+     * @param imageInfoConfig {@link ImageInfoConfig} 对象
+     * @return {@link Picasso} 对象
+     */
+    private Picasso createPicasso(ImageInfoConfig imageInfoConfig) {
+        if (imageInfoConfig.getFragmentV4() != null)
+            return Picasso.with(imageInfoConfig.getFragmentV4().getActivity());
+
+        if (imageInfoConfig.getFragment() != null)
+            return Picasso.with(imageInfoConfig.getFragment().getActivity());
+
+        if (imageInfoConfig.getFragmentActivity() != null)
+            return Picasso.with(imageInfoConfig.getFragmentActivity());
+
+        if (imageInfoConfig.getActivity() != null)
+            return Picasso.with(imageInfoConfig.getActivity());
+
+        if (imageInfoConfig.getContext() != null)
+            return Picasso.with(imageInfoConfig.getContext());
+
+        if (imageInfoConfig.getTarget() != null)
+            return Picasso.with(imageInfoConfig.getTarget().getContext());
+
+        if (application != null)
+            return Picasso.with(application);
+
+        throw new NullPointerException("Picasso 获取不到 Context");
     }
 }
